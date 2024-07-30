28f6b77a1fa2713a5858c9bfbffdadaee002cc00
Nour Eddin Al Shammari <noureddinalshammari@gmail.com>
2024-07-30 10:32:12 -0400

    final commit

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
+''' Executing this function initiates the application of emotion
+    detector to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
 from flask import Flask, render_template, request
@@ -8,13 +8,12 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-    
+    """
+    This function handles text input from user
+    and analyzes it using emotion detector function
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-    
-   
     response = emotion_detector(text_to_analyze)
-
-    
     anger = response['anger']
     disgust = response['disgust']
     joy = response['joy']
@@ -22,22 +21,23 @@ def sent_analyzer():
     fear = response['fear']
     dominant_emotion = response['dominant_emotion']
 
-    
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        
-        return "For the given statement, the system response is:<br />\
-                    Anger : {}<br />\
-                    Disgust: {}<br />\
-                    Fear : {}<br />\
-                    Joy : {}<br />\
-                    Sadness : {}<br />\
-                    The dominant emotion is: {}".format(anger,disgust,fear,joy,sadness,dominant_emotion)
+
+    return f"For the given statement, the system response is:<br />\
+                Anger : {anger}<br />\
+                Disgust: {disgust}<br />\
+                Fear : {fear}<br />\
+                Joy : {joy}<br />\
+                Sadness : {sadness}<br />\
+                The dominant emotion is: {dominant_emotion}"
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the index.html page
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
